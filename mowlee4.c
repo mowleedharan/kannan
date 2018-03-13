e6b0b88565a9f67e340ef1047021e3fea4f75c95
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-03-13 16:29:31 +0530

    Create mowlee4.c

---
+++ mowlee4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char a;
+scanf("%c",&a);
+if(((a>='a')&&(a<='z'))||((a>='A')&&(a<='Z')))
+printf("its an alphabet");
+else 
+printf("its an character");
+}

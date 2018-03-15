024974e136f7a37c6c3bfd2d3e1e7f00e8248207
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-03-15 16:31:33 +0530

    Create 18.c

---
+++ 18.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+int n,remainder,sum=0;
+n=350;
+while(n<=450)
+{
+remainder=n%10;
+sum=sum+(remainder*remainder*remainder);
+n=n/10;
+if(n==sum)
+printf("\n %d is an Armstrong number",n);
+else
+printf("\n %d is not an Armstrong number",n);
+}
+n++;
+return 0;
+}

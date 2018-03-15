64f520015a2a830c2cdd2b21436eb2f6d70da536
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-03-15 16:43:46 +0530

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,28 +1,28 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
 void main()
 {
-int n=0,num,sum=0,rem=0,count=0,n1;
-scanf("%d",&num);
-n1=n=num;
-while(n!=0)
-{
-count++;
-n=n/10;
-}
-while(n1!=0)
-{
-rem=n1%10;
-sum=sum+pow(rem,count);
-n1=n1/10;
-}
-if(sum==num)
-{
-printf("armstrong");
-}
-else
-{
-printf("not an armstrong");
-}
-return 0;
+ int n,count=0,rem,sum=0,temp1,temp2;
+ scanf("%d",&n);
+ temp1=temp2=n;
+ while(temp1>0)
+ {
+   
+     count++;
+     temp1/=10;
+ }
+ while(temp2>0)
+ {
+     rem=temp2%10;
+     sum=sum+pow(rem,count);
+     temp2/=10;
+ }
+ if(sum==n)
+ {
+     printf("Armstrong");
+ }
+ else
+ {
+     printf("Not Armstrong");
+ }
 }

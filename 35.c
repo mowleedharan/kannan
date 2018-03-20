9afe2dcfea8c33eb5e5944e20f6b94e3d6168046
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-03-20 16:44:41 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+   char s[100];
+   int l,count=0,c,i;
+   scanf("%s",s);
+   l=strlen(s);
+   
+   for(i=0;i<l;i++)
+   {
+       if(s[i]>='0' && s[i]<='9')
+       {
+       count++;
+       flag=1;
+       }
+       else
+       flag=0;
+   }
+   if(flag==1)
+      printf("%d",count);
+   else
+      printf("0");
+
+    return 0;
+}
+

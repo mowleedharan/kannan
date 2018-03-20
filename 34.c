24b2111c04f5a2f323cb314dca30c914c634be6a
mowleedharan <35652470+mowleedharan@users.noreply.github.com>
2018-03-20 16:43:04 +0530

    Create 34.c

---
+++ 34.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+void main(void) 
+
+{
+
+	char str[50];
+
+	int i,count=1;
+
+	printf("enter the sentence:");
+
+	scanf("%[^\n]s",str);
+
+	for(i=0;str[i]!='\0';i++)
+
+	{
+
+		if(str[i]=='.')
+
+		{
+
+			count=count+1;
+
+		}
+
+	}
+
+	printf("\nno of lines is=%d",count);
+
+	return 0;
+
+}
